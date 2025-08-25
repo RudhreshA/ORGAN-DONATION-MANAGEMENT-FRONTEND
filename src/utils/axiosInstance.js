8c452e0b72b858b15169c1dc1a41c66840595444
Rudhresh A <rudhresh9606@gmail.com>
2025-08-25 21:33:55 +0530

    Backend url updated in utils folder

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://organconnect-vq06.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
